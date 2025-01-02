c9b508beab1bb6d4a442f71bca4760eb4961d5b0
RamiroEscalante <escalanteramis@gmail.com>
2025-01-02 17:10:01 -0600

    espero que si se el ultmio

--- gif-app/vite.config.js
+++ gif-app/vite.config.js
@@ -1,13 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Esta es la carpeta que Vercel usará como salida
-  },
-});
+})
